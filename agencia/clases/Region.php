3da4799f8c01a8b13ba8665c6f6bdf20b85abc05
Marcos Pinardi <exegeses@gmail.com>
2021-02-25 21:22:09 -0300

    getters & setters

--- agencia/clases/Region.php
+++ agencia/clases/Region.php
@@ -2,5 +2,43 @@
 
     class Region
     {
+        private $regID;
+        private $regNombre;
+
+
+
+        ##########################
+        ## Getters & Setters
+        /**
+         * @return mixed
+         */
+        public function getRegID()
+        {
+            return $this->regID;
+        }
+
+        /**
+         * @param mixed $regID
+         */
+        public function setRegID($regID)
+        {
+            $this->regID = $regID;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getRegNombre()
+        {
+            return $this->regNombre;
+        }
+
+        /**
+         * @param mixed $regNombre
+         */
+        public function setRegNombre($regNombre)
+        {
+            $this->regNombre = $regNombre;
+        }
 
     }

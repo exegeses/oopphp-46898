bac87c2a91090b0aae7f14e9e267021b06fa1658
Marcos Pinardi <exegeses@gmail.com>
2021-02-25 22:02:43 -0300

    panel de administración de regiones

---
+++ agencia/adminRegiones.php
@@ -0,0 +1,56 @@
+<?php
+    require 'config/config.php';
+    require 'clases/Conexion.php';
+    require 'clases/Region.php';
+    $Region = new Region;
+    $regiones = $Region->listarRegiones();
+
+    include 'includes/over-all-header.html';
+    include 'includes/nav.php';
+?>
+
+    <main class="container">
+
+        <h1>Panel de administración de regiones</h1>
+
+        <table class="table table-bodered table-striped table-hover">
+            <thead class="thead-dark">
+            <tr>
+                <th>#</th>
+                <th>Región</th>
+                <th colspan="2">
+                    <a href="/formAgregarRegion" class="btn btn-dark">
+                        Agregar
+                    </a>
+                </th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+                foreach( $regiones as $region ){
+            ?>
+            <tr>
+                <td><?= $region['regID'] ?></td>
+                <td><?= $region['regNombre'] ?></td>
+                <td>
+                    <a href="/formModificarRegion/{{ $region->regID }}" class="btn btn-outline-secondary">
+                        Modificar
+                    </a>
+                </td>
+                <td>
+                    <a href="/formEliminarRegion/{{ $region->regID }}" class="btn btn-outline-secondary">
+                        Eliminar
+                    </a>
+                </td>
+            </tr>
+            <?php
+                    }
+            ?>
+            </tbody>
+        </table>
+
+    </main>
+
+<?php
+    include 'includes/footer.php';
+?>

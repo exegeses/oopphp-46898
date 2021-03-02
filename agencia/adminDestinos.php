ae8023824bdb584f057fff45c4684cb1236ac679
Marcos Pinardi <exegeses@gmail.com>
2021-03-02 19:11:40 -0300

    panel de administración de destinos

--- agencia/adminDestinos.php
+++ agencia/adminDestinos.php
@@ -1,55 +1,53 @@
 <?php
     require 'config/config.php';
-    require 'clases/Conexion.php';
-    require 'clases/Region.php';
-    $Region = new Region;
-    $regiones = $Region->listarRegiones();
-
     include 'includes/over-all-header.html';
     include 'includes/nav.php';
 ?>
 
     <main class="container">
-
-        <h1>Panel de administración de regiones</h1>
+        <h1>Panel de administración de destinos</h1>
 
         <table class="table table-borderless table-striped table-hover">
-            <thead class="thead-dark">
+            <thead>
             <tr>
                 <th>#</th>
+                <th>Nombre de Destino</th>
                 <th>Región</th>
+                <th>Precio</th>
+                <th>Asientos</th>
+                <th>Disponibles</th>
                 <th colspan="2">
-                    <a href="/formAgregarRegion" class="btn btn-dark">
+                    <a href="formAgregarDestino.php" class="btn btn-outline-secondary">
                         Agregar <i class="far fa-plus-square ml-1"></i>
                     </a>
                 </th>
             </tr>
             </thead>
             <tbody>
-            <?php
-                foreach( $regiones as $region ){
-            ?>
+
             <tr>
-                <td><?= $region['regID'] ?></td>
-                <td><?= $region['regNombre'] ?></td>
+                <td> destID </td>
+                <td> destNombre </td>
+                <td> regNombre </td>
+                <td>$ destPrecio </td>
+                <td> destAsientos </td>
+                <td> destDisponibles </td>
                 <td>
-                    <a href="/formModificarRegion/{{ $region->regID }}" class="btn btn-outline-secondary">
+                    <a href="formModificarDestino.php?destID=destID" class="btn btn-outline-secondary">
                         Modificar <i class="far fa-edit ml-1"></i>
                     </a>
                 </td>
                 <td>
-                    <a href="/formEliminarRegion/{{ $region->regID }}" class="btn btn-outline-secondary">
+                    <a href="formEliminarDestino.php?destID=destID" class="btn btn-outline-secondary">
                         Eliminar <i class="far fa-minus-square ml-1"></i>
                     </a>
                 </td>
             </tr>
-            <?php
-                    }
-            ?>
+
             </tbody>
         </table>
 
-    </main>
+        </main>
 
 <?php
     include 'includes/footer.php';
